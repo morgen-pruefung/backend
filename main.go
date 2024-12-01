a394e8bd9a986dea6966480d8274c636dcd7d2ef
Oliver <oschi1303@gmail.com>
2024-12-01 18:10:01 +0100

    Add simple hello world webserver

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.Printf("Starting server...")
+
+	port := mustGetPort()
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	go func() {
+		err := http.ListenAndServe(":"+port, mux)
+		if err != nil {
+			log.Fatalf("error starting server: %v", err)
+			return
+		}
+	}()
+	log.Printf("Server started on port %s", port)
+
+	c := make(chan struct{}, 1) // Block forever
+	<-c
+}
+
+func mustGetPort() string {
+	p := os.Getenv("PORT")
+	if p == "" {
+		return "4242"
+	}
+
+	return p
 }
