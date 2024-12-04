51a55ea8c0eeb642dafd115123c1c32cb43adaad
Oliver <oschi1303@gmail.com>
2024-12-04 15:22:22 +0100

    Add github integration

---
+++ internal/github/github.go
@@ -0,0 +1,55 @@
+package github
+
+import (
+	"os"
+	"os/exec"
+)
+
+const repoDir = "data/repositories/"
+
+func CloneRepo(name, url string) error {
+	dir := repoDir + name
+	if _, err := os.Stat(dir); err == nil {
+		return nil
+	} else if os.IsNotExist(err) {
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	cmd := exec.Command("git", "clone", url)
+	cmd.Dir = dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
+func PullRepo(name string) error {
+	dir := repoDir + name
+	cmd := exec.Command("git", "pull")
+	cmd.Dir = dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
+func ReadFile(name, path string) ([]byte, error) {
+	dir := repoDir + name + "/" + path
+
+	return os.ReadFile(dir)
+}
+
+func ListFiles(name string, subdir string) ([]string, error) {
+	dir := repoDir + name + "/" + subdir
+
+	f, err := os.Open(dir)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return f.Readdirnames(-1)
+}
