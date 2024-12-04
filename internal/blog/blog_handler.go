1ea5792bfa601c2be8b82e35bd57ab08f9f94508
Oliver <oschi1303@gmail.com>
2024-12-04 15:53:11 +0100

    Add blog api

--- internal/blog/blog_handler.go
+++ internal/blog/blog_handler.go
@@ -1,19 +1,21 @@
 package blog
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
-type BlogStore interface {
+type Store interface {
 	GetArticles() ([]Article, error)
 	GetArticle(articleID string) (*Article, error)
 }
 
 type Handler struct {
-	store *BlogStore
+	store Store
 }
 
-func NewHandler(s *BlogStore) *Handler {
+func NewHandler(s Store) *Handler {
 	return &Handler{
 		store: s,
 	}
@@ -25,9 +27,40 @@ func (h *Handler) Register(prefix string, mux *http.ServeMux) {
 }
 
 func (h *Handler) handleGetArticles(w http.ResponseWriter, r *http.Request) {
+	articles, err := h.store.GetArticles()
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("Error getting articles: %v\n", err)
+		return
+	}
 
+	data, err := json.Marshal(articles)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("Error marshalling articles: %v\n", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
 
 func (h *Handler) handleGetArticle(w http.ResponseWriter, r *http.Request) {
+	articleID := r.PathValue("article_id")
+	article, err := h.store.GetArticle(articleID)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("Error getting article: %v\n", err)
+		return
+	}
 
+	data, err := json.Marshal(article)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("Error marshalling article: %v\n", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
--- internal/blog/blogstore/blog_store.go
+++ internal/blog/blogstore/blog_store.go
@@ -1,6 +1,14 @@
 package blogstore
 
-import "backend/internal/blog"
+import (
+	"backend/internal/blog"
+	"backend/internal/github"
+	"encoding/json"
+	"log"
+	"time"
+)
+
+const BibliothekRepo = "bibliothek"
 
 type Store struct {
 }
@@ -9,10 +17,68 @@ func NewStore() *Store {
 	return &Store{}
 }
 
+func StartUpdateTicker() {
+	_, err := github.ListFiles(BibliothekRepo, "")
+	if err != nil {
+		err := github.CloneRepo(BibliothekRepo, "https://github.com/morgen-pruefung/bibliothek.git")
+		if err != nil {
+			log.Fatalf("Error cloning repo: %s\n", err)
+		}
+		return
+	}
+
+	ticker := time.NewTicker(5 * time.Minute)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				err := github.PullRepo(BibliothekRepo)
+				if err != nil {
+					log.Println("Error pulling repo:", err)
+					continue
+				}
+				log.Printf("Pulled repo %s\n", BibliothekRepo)
+			}
+		}
+	}()
+}
+
 func (s *Store) GetArticles() ([]blog.Article, error) {
-	return nil, nil
+	files, err := github.ListFiles(BibliothekRepo, "blog/articles")
+	if err != nil {
+		return nil, err
+	}
+
+	articles := []blog.Article{}
+	for _, file := range files {
+		a, err := s.GetArticle(file)
+		if err != nil {
+			return nil, err
+		}
+		articles = append(articles, *a)
+	}
+
+	return articles, nil
 }
 
 func (s *Store) GetArticle(articleID string) (*blog.Article, error) {
-	return nil, nil
+	metadata, err := github.ReadFile(BibliothekRepo, "blog/articles/"+articleID+"/"+articleID+".json")
+	if err != nil {
+		return nil, err
+	}
+
+	var article blog.Article
+	err = json.Unmarshal(metadata, &article)
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := github.ReadFile(BibliothekRepo, "blog/articles/"+articleID+"/"+articleID+".md")
+	if err != nil {
+		return nil, err
+	}
+
+	article.Body = string(content)
+
+	return &article, nil
 }
