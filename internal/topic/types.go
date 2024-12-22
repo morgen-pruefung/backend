18888a027652843b1d97408540f740b97097e4ab
Oliver <oschi1303@gmail.com>
2024-12-22 16:07:09 +0100

    Add category to topic

--- internal/topic/types.go
+++ internal/topic/types.go
@@ -1,7 +1,8 @@
 package topic
 
 type Topic struct {
-	ID      string `json:"id"`
-	Name    string `json:"name"`
-	Summary string `json:"summary"`
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Summary  string `json:"summary"`
+	Category string `json:"category"`
 }
