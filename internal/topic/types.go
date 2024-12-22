1a202586019fbbbd32f4f5e3435f20b3f2e4c9c0
Oliver <oschi1303@gmail.com>
2024-12-22 14:30:02 +0100

    Add summary to topics

--- internal/topic/types.go
+++ internal/topic/types.go
@@ -1,6 +1,7 @@
 package topic
 
 type Topic struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Summary string `json:"summary"`
 }
